89d09a47f2c619ba5a8791b662c8d0a7074fdb05
Shute <graham.shute@capgemini.com>
2018-03-27 11:16:32 +0100

    Parameter Syntax change

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -2,15 +2,15 @@ param (
     [Parameter(Mandatory=$true)][string]$DeploymentName="G-Template",
     [Parameter(Mandatory=$true)][string]$Regurl="",
     [Parameter(Mandatory=$true)][string]$RegistrationKey="",
-    [Parameter(Mandatory=$true)][string]$AccountDetails=""
+    [Parameter(Mandatory=$true)][string]$AccountID=""
 )
 
 
-.\azuredeploy.ps1 -DeploymentName CAPDemo -Regurl $RegistrationInfo.Endpoint  -RegistrationKey $RegistrationInfo.primarykey -AccountID $AccountDetails.resourceid 
+#.\azuredeploy.ps1 -DeploymentName CAPDemo -Regurl $RegistrationInfo.Endpoint  -RegistrationKey $RegistrationInfo.primarykey -AccountID $AccountDetails.resourceid 
 
 
 new-azurermresourcegroup -name ($DeploymentName) -location "West US"
 
 new-azurermresourcegroup -name ($DeploymentName + "-NET") -location "West US"
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $DeploymentName -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
+New-AzureRmResourceGroupDeployment -ResourceGroupName $DeploymentName -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json  -Regurl $RegistrationInfo.Endpoint  -RegistrationKey $RegistrationInfo.primarykey -AccountID $AccountDetails.resourceid 
